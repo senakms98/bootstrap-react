65c0948cace7ee4e6dfa435d4fd57fd562cef164
Sena Okumuş <100844463+senakms98@users.noreply.github.com>
2024-01-03 23:33:30 +0300

    navbar first try

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './style.scss'
+import Navbar from "../sections/Navbar.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    <Navbar />
+  </React.StrictMode>
 )
